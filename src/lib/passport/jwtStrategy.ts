029d967b3758f8bf9b52c2768a4dcb4441e951d1
준오 <juno@jun-oui-noteubug.local>
2025-12-04 16:02:33 +0900

    환경변수 상수화 파일 작성

---
+++ src/lib/passport/jwtStrategy.ts
@@ -0,0 +1,1 @@
+import { Strategy as JWTStrategy, ExtractJwt } from "passport-jwt";
--- src/lib/passport/localStrategy.ts
+++ src/lib/passport/localStrategy.ts
@@ -1,33 +1,33 @@
-import { Strategy as LocalStrategy } from "passport-local";
-import prisma from "../prisma.js";
-import bcrypt from "bcrypt";
-import {AuthService} from "./authService.ts";
+import { Strategy as LocalStrategy } from 'passport-local';
+import prisma from '../prisma.js';
+import bcrypt from 'bcrypt';
+import { AuthService } from './authService.ts';
+import { User } from '@prisma/client';
 
 const authService = new AuthService(prisma); // 의존성 주입
 
-type veriftCb = (error: any, user?:any, info?: any ) => void;
+type verifyCb = (error: Error | null, user?: User | false, info?: { message: string }) => void;
 
 export const localStrategy = new LocalStrategy(
-	{
-		usernameField: "email",
-		passwordField: "password"
-	}
-	async (email: string, password: string, cb: verifyCb) =>{
-		try{
-			const user = await prisma.user.findUnique({
-				where: email
-			})
-			if (!user || !user.password) return cb( null, false, { message: "해당 유저가 존재하지 않습니다.."})
-
-			const isMatched = await bcrypt.compare(password, user.password);
-			if(!isMatched){
-			       	return cb(null, false,{ message: "잘못된 비밀번호입니다."});
-			}else{
-				cb(null, user)
-			}
-
-		}catch(error){
-			cb(error)
-		}
-	}
-)
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+  },
+  async (email: string, password: string, cb: verifyCb) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
+      if (!user || !user.password)
+        return cb(null, undefined, { message: '해당 유저가 존재하지 않습니다..' });
+      const isMatched = await bcrypt.compare(password, user.password);
+      if (!isMatched) {
+        return cb(null, undefined, { message: '잘못된 비밀번호입니다.' });
+      } else {
+        cb(null, user);
+      }
+    } catch (error) {
+      cb(error as Error);
+    }
+  },
+);
