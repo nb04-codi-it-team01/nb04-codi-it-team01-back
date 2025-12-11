f2633c71e085a9d483b9439cd5e83251a1981fc4
준오 <juno@jun-oui-noteubug.local>
2025-12-11 16:39:43 +0900

    mv upload handler to middle ware

--- src/app.ts
+++ src/app.ts
@@ -8,51 +8,8 @@ import userRoute from './features/user/user.route';
 import storeRoute from './features/store/store.route';
 import { requestLogger } from './shared/middleware/logger';
 import { errorHandler } from './shared/middleware/error-handler';
-import Multer from 'multer';
-import fs from 'fs';
-import path from 'path';
-import type { Request } from 'express';
-import { FileFilterCallback } from 'multer';
-import multer from 'multer';
-
+import { upload } from './shared/middleware/upload-handler';
 export const app = express();
-
-type fileNameCallback = (error: Error | null, filename: string) => void;
-type destinationCallback = (error: Error | null, result: string) => void;
-const storage = Multer.diskStorage({
-  destination: (req: Request, file: Express.Multer.File, cb: destinationCallback): void => {
-    const uploadPath = 'upload/';
-    if (!fs.existsSync(uploadPath)) {
-      fs.mkdirSync(uploadPath);
-    }
-    cb(null, uploadPath);
-  },
-  filename: (req: Request, file: Express.Multer.File, cb: fileNameCallback): void => {
-    const ext = path.extname(file.originalname);
-    const filename = file.fieldname + '-' + Date.now() + ext;
-    cb(null, filename);
-  },
-});
-export const fileFilter = (
-  req: Request,
-  file: Express.Multer.File,
-  callback: FileFilterCallback,
-): void => {
-  if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-  ) {
-    callback(null, true);
-  } else {
-    callback(null, false);
-  }
-};
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1024 * 1024 * 2 },
-  fileFilter,
-});
 
 app.use(cookieParser());
 // CORS 설정 - 프론트엔드(3000)에서 접근 허용
