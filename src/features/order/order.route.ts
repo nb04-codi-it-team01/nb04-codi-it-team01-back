fd640a466a30139f9f6186f6ac8571f791c7bf73
Batur-s <bill0302@naver.com>
2025-12-19 09:15:52 +0900

    fix: mock 테스트 파츠 삭제 및 .env.sample 재생성

--- src/features/order/order.route.ts
+++ src/features/order/order.route.ts
@@ -12,19 +12,3 @@ router.delete('/orders/:orderId', accessTokenAuth, controller.deleteOrder);
 router.patch('/orders/:orderId', accessTokenAuth, controller.updateOrder);
 
 export default router;
-
-// 기능 테스트를 위한 mock 우회 코드
-// import { Router } from 'express';
-// import { OrderController } from './order.controller';
-// import { mockAuth } from '../../shared/middleware/mockAuth';
-
-// const router = Router();
-// const controller = new OrderController();
-
-// router.get('/orders', mockAuth, controller.getOrder);
-// router.post('/orders', mockAuth, controller.createOrder);
-// router.get('/orders/:orderId', mockAuth, controller.getOrderDetail);
-// router.delete('/orders/:orderId', mockAuth, controller.deleteOrder);
-// router.patch('/orders/:orderId', mockAuth, controller.updateOrder);
-
-// export default router;
